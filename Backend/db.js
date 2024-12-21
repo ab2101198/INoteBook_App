c56eb8dcf5d172f3f259f7d12d24ffad1a4ec9c7
ab2101198 <116869072+ab2101198@users.noreply.github.com>
2024-12-21 18:49:25 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose') ;
 
 
-const mongoURI = "mongodb://0.0.0.0:27017/inotebook" 
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook" 
 
 const connectToMongo = () => {
       mongoose.connect(mongoURI).then
